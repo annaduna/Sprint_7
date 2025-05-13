d3acc41adaf42ff830ec30de53c422986d6a1d56
Ann Dunaieva <dunaievaann@gmail.com>
2025-05-13 14:58:10 +0300

    Сделаны изменения после ревью

--- test_courier/test_create_courier.py
+++ test_courier/test_create_courier.py
@@ -3,39 +3,41 @@ from method.courier_method import CourierMethod
 from data import DataForCourier
 from generators import generate_courier
 
-@allure.description("Тест проверяет, что курьер успешно создаётся и возвращает правильный код ответа и тело ответа. Все обязательные поля переданы. ")
-def test_create_courier_success(courier_cleanup):
-    response = CourierMethod.create_courier(DataForCourier.CREATE_COURIER)
-    assert response.status_code == 201
-    response_json = response.json()
-    assert response_json.get("ok") is True
+class TestcreateCourier:
 
-    courier_id = CourierMethod.get_courier_id(DataForCourier.CREATE_COURIER)
-    courier_cleanup(courier_id)
+    @allure.description("Тест проверяет, что курьер успешно создаётся и возвращает правильный код ответа и тело ответа. Все обязательные поля переданы. ")
+    def test_create_courier_success(self,courier_cleanup):
+        response = CourierMethod.create_courier(DataForCourier.CREATE_COURIER)
+        assert response.status_code == 201
+        response_json = response.json()
+        assert response_json.get("ok") is True
 
-@allure.description("Тест проверяет, что нельзя создать двух курьеров с одинаковыми данными. Также проверка на то что если создать пользователя с логином, который уже есть, возвращается ошибка.")
-def test_create_duplicate_courier(courier_cleanup):
+        courier_id = CourierMethod.get_courier_id(DataForCourier.CREATE_COURIER)
+        courier_cleanup(courier_id)
 
-    response = CourierMethod.create_courier(DataForCourier.CREATE_COURIER)
-    assert response.status_code == 201
-    courier_id = CourierMethod.get_courier_id(DataForCourier.CREATE_COURIER)
+    @allure.description("Тест проверяет, что нельзя создать двух курьеров с одинаковыми данными. Также проверка на то что если создать пользователя с логином, который уже есть, возвращается ошибка.")
+    def test_create_duplicate_courier(self,courier_cleanup):
 
-    courier_cleanup(courier_id)
+        response = CourierMethod.create_courier(DataForCourier.CREATE_COURIER)
+        assert response.status_code == 201
+        courier_id = CourierMethod.get_courier_id(DataForCourier.CREATE_COURIER)
 
-    second_response = CourierMethod.create_courier(DataForCourier.CREATE_COURIER)
-    assert second_response.status_code == 409
+        courier_cleanup(courier_id)
 
-    second_create_courier_text = second_response.json()
-    assert second_create_courier_text.get("message") == "Этот логин уже используется. Попробуйте другой."
+        second_response = CourierMethod.create_courier(DataForCourier.CREATE_COURIER)
+        assert second_response.status_code == 409
 
-@allure.description("Тест проверяет, что запрос без обязательного поля возвращает ошибку 400.")
-def test_create_courier_without_login():
-    courier_data = generate_courier()
-    courier_data.pop("login")
-    response = CourierMethod.create_courier(courier_data)
-    assert response.status_code == 400
-    response_json = response.json()
-    assert response_json.get("message") == "Недостаточно данных для создания учетной записи"
+        second_create_courier_text = second_response.json()
+        assert second_create_courier_text.get("message") == "Этот логин уже используется. Попробуйте другой."
+
+    @allure.description("Тест проверяет, что запрос без обязательного поля возвращает ошибку 400.")
+    def test_create_courier_without_login(self):
+        courier_data = generate_courier()
+        courier_data.pop("login")
+        response = CourierMethod.create_courier(courier_data)
+        assert response.status_code == 400
+        response_json = response.json()
+        assert response_json.get("message") == "Недостаточно данных для создания учетной записи"
 
 
 
